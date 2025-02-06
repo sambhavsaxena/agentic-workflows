6720e137f849ad3abdf09a46cf63102ef704dcda
Sambhav Saxena <sambhavsaxena02@gmail.com>
2025-02-06 19:06:23 +0530

    fix: cors headers added on server req
    Signed-off-by: Sambhav Saxena <sambhavsaxena02@gmail.com>

--- finance-agent/index.js
+++ finance-agent/index.js
@@ -49,6 +49,14 @@ const log_message = (message) => {
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 
 const messages = [{ role: "developer", content: DEVELOPER_PROMPT }];
 
